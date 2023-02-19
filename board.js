223e2234380103f93efafc9430bd2f84d6f4b50a
Kiril Rumyantsev <rumyantsev@scand.com>
2023-02-19 20:35:42 +0300

    feat: matrix board

--- board.js
+++ board.js
@@ -4,14 +4,14 @@ export default class Board {
     whiteCheckers = [];
     blackCheckers = [];
     board = [
-        null, 0, null, 1, null, 2, null, 3,
-        4, null, 5, null, 6, null, 7, null,
-        null, 8, null, 9, null, 10, null, 11,
-        null, null, null, null, null, null, null, null,
-        null, null, null, null, null, null, null, null,
-        12, null, 13, null, 14, null, 15, null,
-        null, 16, null, 17, null, 18, null, 19,
-        20, null, 21, null, 22, null, 23, null
+        [null, 0, null, 1, null, 2, null, 3],
+        [4, null, 5, null, 6, null, 7, null],
+        [null, 8, null, 9, null, 10, null, 11],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [12, null, 13, null, 14, null, 15, null],
+        [null, 16, null, 17, null, 18, null, 19],
+        [20, null, 21, null, 22, null, 23, null]
     ];
 
     init() {
@@ -33,13 +33,15 @@ export default class Board {
         let k = 0;
         let j = 0;
         for (let i = 0; i < this.board.length; i++) {
-            if (this.board[i] < 12 && this.board[i] != null) {
-                this.board[i] = this.whiteCheckers[k];
-                k++;
-            } else {
-                if (this.board[i] >= 12 && this.board[i] != null) {
-                    this.board[i] = this.blackCheckers[j];
-                    j++;
+            for(let f = 0; f < this.board[i].length; f++) {
+                if (this.board[i][f] < 12 && this.board[i][f] != null) {
+                    this.board[i][f] = this.whiteCheckers[k];
+                    k++;
+                } else {
+                    if (this.board[i][f] >= 12 && this.board[i][f] != null) {
+                        this.board[i][f] = this.blackCheckers[j];
+                        j++;
+                    }
                 }
             }
         }
@@ -48,8 +50,10 @@ export default class Board {
     getBoardIndex(id) {
         id = +id;
         for(let i = 0; i < this.board.length; i++) {
-            if(this.board[i] != null && this.board[i].divId === id) {
-                return this.board[i].position;
+            for(let j = 0; j < this.board[i].length; j++) {
+                if(this.board[i][j] != null && this.board[i][j].divId === id) {
+                    return this.board[i][j].position;
+                }
             }
         }
         return 0;
