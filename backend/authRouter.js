a7f67bb05092d84e72c5dc56f51dae88289a46ca
rumyantsev <rumyantsev@scand.com>
2023-03-09 13:11:10 +0300

    problem: roles not working

--- backend/authRouter.js
+++ backend/authRouter.js
@@ -1,9 +1,15 @@
 const Router = require("express");
 const controller = require("./controllers/authController.js")
 const router = new Router();
+const {check} = require("express-validator");
+const authMiddleWare = require("./middleware/authMiddleware")
+const roleMiddleware = require("./middleware/roleMiddleware")
 
-router.post("/registration", controller.registration);
+router.post("/registration", [
+    check("username","Имя пользователя не может быть пустым").notEmpty(),
+    check("password", "Пароль должен быть длиннее 7 символов и короче 15").isLength({min:7,max:15})
+], controller.registration);
 router.post("/login", controller.login);
-router.get("/users", controller.getUsers)
+router.get("/users", roleMiddleware(["ADMIN"]), controller.getUsers);
 
 module.exports = router
