164c53dbe5d0339365158a048a5a772ad5898e31
krumyantsev-sc <rumyantsev@scand.com>
2023-03-16 12:37:53 +0300

    feat: add connect buttons(front)

--- roomList.js
+++ roomList.js
@@ -14,7 +14,18 @@ createRoomButton.addEventListener("click", () => {
 async function getLobbyList() {
     let res = await get("http://localhost:3001/room/getRoomList");
     let lobbyArr = await res;
-    let newCheckerDiv = document.createElement('div');
-    lobbyArr.forEach()
+    lobbyArr.forEach((el) => createRoomDiv(el));
+}
+
+function createRoomDiv(roomInfo) {
+    let newRoomDiv = document.createElement('div');
+    newRoomDiv.classList.add("roomDiv");
+    newRoomDiv.textContent = `room ID: ${roomInfo._id}, player 1: ${roomInfo.firstPlayerId}, player2: ${roomInfo.secondPlayerId}`;
+    let newRoomBtn = document.createElement('div');
+    newRoomBtn.classList.add("roomDivBtn");
+    newRoomBtn.textContent = "CONNECT";
+    newRoomDiv.appendChild(newRoomBtn);
+    roomList.appendChild(newRoomDiv);
+
 }
 console.log(getLobbyList());
