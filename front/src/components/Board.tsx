326a487f50ea1483e3fd4eef6a13a0d563bcb2c6
krumyantsev-sc <rumyantsev@scand.com>
2023-04-06 17:42:34 +0300

    feat: add board component

---
+++ front/src/components/Board.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import "../styles/Board.css"
+
+const Board = () => {
+    return (
+        <div>
+            <table>
+                <tr>
+                    <td className="cleanCell"/>
+                    <td/>
+                    <td className="cleanCell"/>
+                    <td/>
+                    <td className="cleanCell"/>
+                    <td/>
+                    <td className="cleanCell"/>
+                    <td/>
+                </tr>
+                <tr>
+                    <td/>
+                    <td className="cleanCell"/>
+                    <td/>
+                    <td className="cleanCell"/>
+                    <td/>
+                    <td className="cleanCell"/>
+                    <td/>
+                    <td className="cleanCell"/>
+                </tr>
+                <tr>
+                    <td className="cleanCell"/>
+                    <td/>
+                    <td className="cleanCell"/>
+                    <td/>
+                    <td className="cleanCell"/>
+                    <td/>
+                    <td className="cleanCell"/>
+                    <td/>
+                </tr>
+                <tr>
+                    <td/>
+                    <td className="cleanCell"/>
+                    <td/>
+                    <td className="cleanCell"/>
+                    <td/>
+                    <td className="cleanCell"/>
+                    <td/>
+                    <td className="cleanCell"/>
+                </tr>
+                <tr>
+                    <td className="cleanCell"/>
+                    <td/>
+                    <td className="cleanCell"/>
+                    <td/>
+                    <td className="cleanCell"/>
+                    <td/>
+                    <td className="cleanCell"/>
+                    <td/>
+                </tr>
+                <tr>
+                    <td/>
+                    <td className="cleanCell"/>
+                    <td/>
+                    <td className="cleanCell"/>
+                    <td/>
+                    <td className="cleanCell"/>
+                    <td/>
+                    <td className="cleanCell"/>
+                </tr>
+                <tr>
+                    <td className="cleanCell"/>
+                    <td/>
+                    <td className="cleanCell"/>
+                    <td/>
+                    <td className="cleanCell"/>
+                    <td/>
+                    <td className="cleanCell"/>
+                    <td/>
+                </tr>
+                <tr>
+                    <td/>
+                    <td className="cleanCell"/>
+                    <td/>
+                    <td className="cleanCell"/>
+                    <td/>
+                    <td className="cleanCell"/>
+                    <td/>
+                    <td className="cleanCell"/>
+                </tr>
+            </table>
+        </div>
+    );
+};
+
+export default Board;
